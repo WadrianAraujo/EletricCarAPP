7de9ca49947d365408ae840f1673645f35819ccc
Wadrian Araujo De Moura <wadm.snf20@uea.edu.br>
2023-10-14 16:23:52 -0400

    progress of class 3

---
+++ app/src/main/java/com/example/eletriccarapp/presentation/CalculateAutonomyActivity.kt
@@ -0,0 +1,13 @@
+package com.example.eletriccarapp.presentation
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.eletriccarapp.R
+
+class CalculateAutonomyActivity : AppCompatActivity(){
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_calculate_autonomy)
+    }
+}
--- app/src/main/java/com/example/eletriccarapp/presentation/MainActivity.kt
+++ app/src/main/java/com/example/eletriccarapp/presentation/MainActivity.kt
@@ -1,15 +1,17 @@
-package com.example.eletriccarapp
+package com.example.eletriccarapp.presentation
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import com.example.eletriccarapp.R
 
 class MainActivity : AppCompatActivity() {
     private lateinit var price : EditText
     private lateinit var kmTraveled : EditText
-    private lateinit var btnCalculate : Button
+    private lateinit var btnRedirect : Button
     private lateinit var result : TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -21,15 +23,15 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun setupViews(){
-        btnCalculate = findViewById(R.id.btn_calculate)
-        kmTraveled = findViewById(R.id.et_km)
-        price = findViewById(R.id.et_price)
-        result = findViewById(R.id.tv_result)
+        btnRedirect = findViewById(R.id.btn_Redirect)
+        //kmTraveled = findViewById(R.id.et_km)
+        //price = findViewById(R.id.et_price)
+        //result = findViewById(R.id.tv_result)
     }
 
     fun setupListeners(){
-        btnCalculate.setOnClickListener {
-            Calculate()
+        btnRedirect.setOnClickListener {
+            startActivity(Intent(this,CalculateAutonomyActivity::class.java))
         }
     }
 
