b66ab421a420767697fde8784baf052d4d5df41a
Wadrian Araujo De Moura <wadm.snf20@uea.edu.br>
2023-10-19 13:50:59 -0400

    add verification of no connection internet

--- app/src/main/java/com/example/eletriccarapp/presentation/CarFragment.kt
+++ app/src/main/java/com/example/eletriccarapp/presentation/CarFragment.kt
@@ -1,14 +1,21 @@
 package com.example.eletriccarapp.presentation
 
+import android.content.Context
 import android.content.Intent
+import android.net.ConnectivityManager
+import android.net.NetworkCapabilities
 import android.os.AsyncTask
+import android.os.Build
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.ProgressBar
+import android.widget.TextView
+import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.RecyclerView
 import com.example.eletriccarapp.R
@@ -29,6 +36,8 @@ class CarFragment : Fragment() {
     private lateinit var btnRedirect: FloatingActionButton
     private lateinit var carList: RecyclerView
     private lateinit var progressBar: ProgressBar
+    private lateinit var noInternetImage : ImageView
+    private lateinit var noInternetText : TextView
 
     var carsArray: ArrayList<Car> = ArrayList()
 
@@ -44,15 +53,27 @@ class CarFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
         setupView(view)
-        callService()
         setupListeners()
+        if (checkForInternet(context)){
+            callService()
+        }else{
+            showEmptyState()
+        }
+    }
+
+    fun showEmptyState(){
+        carList.isVisible = false
+        progressBar.isVisible = false
+        noInternetText.isVisible = true
+        noInternetImage.isVisible = true
     }
 
     fun setupView(view: View) {
         btnRedirect = view.findViewById(R.id.fab_calculate)
         carList = view.findViewById(R.id.rv_list_cars)
         progressBar = view.findViewById(R.id.pb_loader)
-
+        noInternetImage = view.findViewById(R.id.iv_empty_state)
+        noInternetText = view.findViewById(R.id.tv_no_internet)
     }
 
     fun setupList() {
@@ -70,6 +91,28 @@ class CarFragment : Fragment() {
     fun callService() {
         val urlbase = "https://igorbag.github.io/cars-api/cars.json"
         MyTask().execute(urlbase)
+    }
+
+    fun checkForInternet(context: Context?): Boolean {
+        val connectivityManager =
+            context?.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            val network = connectivityManager.activeNetwork ?: return false
+
+            val activeNetwork = connectivityManager.getNetworkCapabilities(network) ?: return false
+
+            return when {
+                activeNetwork.hasTransport(NetworkCapabilities.TRANSPORT_WIFI) -> true
+                activeNetwork.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR) -> true
+                else -> false
+            }
+        } else {
+            @Suppress("DEPRECATION")
+            val networkInfo = connectivityManager.activeNetworkInfo ?: return false
+            @Suppress("DEPRECATION")
+            return networkInfo.isConnected
+        }
     }
 
     inner class MyTask : AsyncTask<String, String, String>() {
@@ -95,11 +138,11 @@ class CarFragment : Fragment() {
 
                 val responseCode = urlConnection.responseCode
 
-                if (responseCode == HttpURLConnection.HTTP_OK){
+                if (responseCode == HttpURLConnection.HTTP_OK) {
                     var response = urlConnection.inputStream.bufferedReader().use { it.readText() }
                     publishProgress(response)
-                }else{
-                    Log.e("Erro","Serviço indisponivel no momento")
+                } else {
+                    Log.e("Erro", "Serviço indisponivel no momento")
                 }
 
 
